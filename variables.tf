1f9ad7ba1a35bce1ed00c24ec09dd21d96625194
Khoa <khoa150899@gmail.com>
2024-11-25 22:49:14 -0800

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "azure_resource_group" {
+    type = string
+    default = ""
+}
+
+variable "azure_storage_account" {
+    type = string
+    default = ""
+}
+
+variable "azure_storage_container" {
+    type = string
+    default = ""
+}
+
+variable "azure_storage_blob" {
+    type = string
+    default = ""
+}
